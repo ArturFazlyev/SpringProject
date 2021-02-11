b5580b0210a34cd351a5ea8710759fc4762e2ba9
Артур <fazlyev.art@gmail.com>
2021-02-11 22:39:00 +0500

    Initial commit

---
+++ src/main/java/spring/project/TestBean.java
@@ -0,0 +1,15 @@
+package spring.project;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TestBean {
+    String name;
+
+
+}
